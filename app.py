a777033bac2de9198dbb34e8d89513909551d225
humam.kourani <humam.kourani@fit.fraunhofer.de>
2024-03-18 13:09:55 +0100

    added reset button

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
+import copy
 import os
 import base64
-import openai
 from flask import Flask, render_template, request, flash, redirect, url_for, jsonify
 from utils.conversation import create_conversation, create_message
 
@@ -19,7 +19,9 @@ def index():
         session['api_key'] = request.form.get('api_key')
         try:
             response, conversation = generate_response_with_history(create_conversation(), session['api_key'], session['model_name'])
-            session['initial_conversation'] = conversation
+            session['initial_conversation_without_model'] = conversation
+            session.pop('conversation', None)
+            session.pop('initial_conversation', None)
             return render_template('upload.html')
         except Exception as e:
             flash(str(e), 'error')
@@ -34,7 +36,9 @@ def upload_bpmn():
             if file.filename.endswith('.bpmn'):
                 bpmn_content = file.read().decode('utf-8')
                 bpmn_content_base64 = base64.b64encode(bpmn_content.encode('utf-8')).decode('utf-8')
+                session['initial_conversation'] = copy.deepcopy(session['initial_conversation_without_model'])
                 session['initial_conversation'].append(create_message(f'This is the XML of content of the BPMN file: {bpmn_content}'))
+                session.pop('conversation', None)
                 return render_template('upload.html', bpmn_content_base64=bpmn_content_base64)
             else:
                 flash('Unsupported file type. Please upload a .bpmn file.', 'error')
@@ -51,7 +55,7 @@ def chat_with_llm():
     data = request.json
     user_message = data.get('message', '')
     if 'conversation' not in session:
-        session['conversation'] = session['initial_conversation']
+        session['conversation'] = copy.deepcopy(session['initial_conversation'])
 
     session['conversation'].append({"role": "user", "content": user_message})
 
@@ -61,17 +65,16 @@ def chat_with_llm():
     try:
         new_message, updated_history = generate_response_with_history(session['conversation'], api_key, openai_model)
         session['conversation'] = updated_history 
+        print(updated_history)
         return jsonify({"response": new_message})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
 @app.route('/reset_conversation', methods=['POST'])
 def reset_conversation():
-    if 'initial_conversation' in session:
-        session['conversation'] = session.get('initial_conversation', [])
-    else:
-        session.pop('conversation', None) 
+    session.pop('conversation', None)
     return jsonify({"success": "Conversation has been reset"}), 200
+
     
 
 
--- utils/openai_connection.py
+++ utils/openai_connection.py
@@ -53,20 +53,18 @@ def generate_response_with_history(conversation_history, api_key, openai_model) 
         response = client.chat.completions.create(model=openai_model, messages=messages_payload)
         try:
             new_message = response.choices[0].message.content
-            conversation_history.append({"role": "user", "content": new_message})
-            print('HIST', conversation_history)
+            conversation_history.append({"role": "system", "content": new_message})
             return new_message, conversation_history
         except Exception as e:
-            raise Exception(f"Connection to OpenAI failed! {e}. This is the response: " + str(response))
+            raise Exception(f"Connection to OpenAI failed! This is the response: " + str(response))
 
     else:
         response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload).json()
         try:
             new_message = response["choices"][0]["message"]["content"]
-            conversation_history.append({"role": "user", "content": new_message})
-            print('HIST', conversation_history)
+            conversation_history.append({"role": "system", "content": new_message})
             return new_message, conversation_history
         except Exception as e:
-            raise Exception(f"Connection to OpenAI failed! {e}. This is the response: " + str(response))
+            raise Exception(f"Connection to OpenAI failed! This is the response: " + str(response))
 
     
