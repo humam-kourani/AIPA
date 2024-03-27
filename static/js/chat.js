196e6ce6380d757eccdbb2d56113619843c16625
humam.kourani <humam.kourani@fit.fraunhofer.de>
2024-03-27 14:57:58 +0100

    style changes and error handling

--- static/js/chat.js
+++ static/js/chat.js
@@ -7,9 +7,6 @@ function sendMessage() {
         .then(function (response) {
             var llmResponse = response.data.response;
             appendMessage("LLM", llmResponse);
-        })
-        .catch(function (error) {
-            console.error(error);
         });
 }
 
@@ -20,25 +17,3 @@ function appendMessage(sender, message) {
     chatBox.appendChild(messageElement);
     chatBox.scrollTop = chatBox.scrollHeight; // Scroll to bottom
 }
-
-$(document).ready(function () {
-    $("#sidebar").mCustomScrollbar({
-        theme: "minimal"
-    });
-
-    $('#dismiss, .overlay').on('click', function () {
-        // hide sidebar
-        $('#sidebar').removeClass('active');
-        // hide overlay
-        $('.overlay').removeClass('active');
-    });
-
-    $('#sidebarCollapse').on('click', function () {
-        // open sidebar
-        $('#sidebar').addClass('active');
-        // fade in the overlay
-        $('.overlay').addClass('active');
-        $('.collapse.in').toggleClass('in');
-        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-    });
-});
